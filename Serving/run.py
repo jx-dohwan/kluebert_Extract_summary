1c5ff18eae43a1469d4f4e78464fbe75d14021bb
jx-dohwan <79912056+jx-dohwan@users.noreply.github.com>
2023-03-11 11:27:37 +0900

    Add files via upload

--- Serving/run.py
+++ Serving/run.py
@@ -5,13 +5,11 @@ from preprocessor import preprocess_sentence, preprocess_result, remove_empty_pa
 import math
 import time
 import sys
-sys.path.append("")
+sys.path.append("C:\pythonStudy\추출요약\SRC")
+from train import new_inference
 
-
+test_from = "MODEL/KLUE/bert_transformer/model_step_1000.pt"
 app = Flask(__name__)
-model_name = "jx7789/kobart_summary_v3"
-gen_kwargs = {"length_penalty": 1.2, "num_beams":8, "max_length": 128}
-
 
 @app.route('/', methods=['GET','POST'])
 
@@ -22,18 +20,7 @@ def home():
     # 리스트 형식X 전체 데이터를 넣어서 '\n'.join으로 받기
     # 추출요약 모델 SRC 파일로 결과 출력까지 거의 그대로 구현
     text_input = "\n".join(text_input)
-    if text_input: 
-
-        result = preprocess_sentence(text_input)
-        result = remove_empty_pattern(result)
-        result = preprocess_result(result) #내 생각과는 다르게 사용을 하지 않아도 [이름][시간]다 제거되었음, 
-        #start = time.time()
-        #print(result)
-        pipe = pipeline("summarization", model=model_name)
-        text_output = pipe('[sep]'.join(result), **gen_kwargs)[0]["summary_text"]
-        #end = time.time()
-        #print("걸린시간",end - start)
-        #print("test하기",text_output)
+    text_output = new_inference(text_input, test_from, "transformer", "0", "0",1)
     return render_template('index.html', text_output=text_output)
 
 if __name__ == '__main__':
